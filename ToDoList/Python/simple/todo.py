8e00a96dfbcf4df8036740162a43ad64e35018bd
Peter O Leary <peter.ole2020@outlook.ie>
2025-04-14 14:25:10 +0100

    Created "add task" function. needs to be tested
    added basic main menu. functionality will be added with more functions later on

--- ToDoList/Python/simple/todo.py
+++ ToDoList/Python/simple/todo.py
@@ -1,28 +1,64 @@
+import datetime
+import json
+import os
+
+TASK_FILE = "tasks.json"
+
+if os.path.exists(TASK_FILE):
+    print("File does exist!")
+    pass
+else:
+    print("File does NOT exist!")
+    file = open(TASK_FILE, "w")
+    file.close()
 
 # Add a task
+task_id = 0
 
-tasks_dictionary = {
+def add_tasks(task_id):
 
-}
+    # task ID
 
-def add_tasks():
+    task_id += 1
 
-    task_desc = input("Description: ")
-    task_priority = input("Priority: ")
+    # task description
 
-    task_id = len(tasks_dictionary) + 1
+    task_description = input("Description: ")
 
-    tasks_dictionary[task_id] = {
-        'task': task_desc,
-        'completed': False,
-        'priority': task_priority
-    }
+    # task status
 
-    print(f"{task_desc} has been added with ID {task_id}")
+    task_status = False
+
+    if task_status == False:
+        status = "Incomplete!"
+
+    else:
+        status = "Complete!"
+
+
+    # task created at
+
+    created_at = datetime.now()
+
+    # task updated at
+
+    updated_at = created_at
+
+    # task populating
+
+    task_list = [("ID: " + task_id), ("Decrription: " + task_description), 
+            ("Status: " + task_status), ("Created At: " + created_at), 
+            ("Updated At" + updated_at)]
+
+    file = open(TASK_FILE, "a")
+    for task in task_list:
+        file.write(task_list[task])
+    file.write("\n")
+    file.close()
 
 # View a task
 
-def view_task():
+def update_task():
     pass
 
 # Remove a task
@@ -32,12 +68,45 @@ def remove_task():
 
 # Edit status of a task
 
-def edit_status_task():
+def list_tasks_all():
     pass
 
-# Exit program
+def list_completed_tasks():
+    pass
+
+def list_incomplete_tasks():
+    pass
 
 def exit_program():
     pass
 
+program_menu = ["1: Add Task", "2: Update Task", "3: Delete Task", 
+                "4: List All Tasks", "5: List All Completed Tasks",
+                "6: List All Non-Completed Tasks", "7: Exit Program"]
+
+for item in range(len(program_menu)):
+    print(program_menu[item])
+    item +=1
+
+menu_input = int(input("Menu Selection: "))
+
+if menu_input == 1:
+    add_tasks()
+
+elif menu_input == 2:
+    update_task()
+
+elif menu_input == 3:
+    remove_task()
+
+elif menu_input == 4:
+    list_tasks_all()
+
+elif menu_input == 5:
+    list_completed_tasks()
+
+elif menu_input == 6:
+    list_incomplete_tasks()
+
+elif
 # End
